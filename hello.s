e88910ae28732d84f0573ca5df053ef90a6b41aa
Konstantin Astafurov <40547702+konst-aa@users.noreply.github.com>
2025-05-11 00:21:17 +0000

    ok confirmation stuff works

--- hello.s
+++ hello.s
@@ -1,8 +1,9 @@
 .data
-msg:    .asciz "Hello"
+msg:    .asciz "Hello World!\n"
 
 .text
 .globl main
 main:
     li a7, 4         # syscall print string
     la a0, msg       # load address of string into a0
+    ecall
