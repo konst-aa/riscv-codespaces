1a194007e54509ed772cd6dcb4ea542b116e7ffd
Konstantin Astafurov <40547702+konst-aa@users.noreply.github.com>
2025-05-29 19:26:01 +0000

    add defaults

--- Makefile
+++
@@ -1,2 +0,0 @@
-hello:
-	Rars hello.s
--- hello.s
+++
@@ -1,9 +0,0 @@
-.data
-msg:    .asciz "Hello World!\n"
-
-.text
-.globl main
-main:
-    li a7, 4         # syscall print string
-    la a0, msg       # load address of string into a0
-    ecall
